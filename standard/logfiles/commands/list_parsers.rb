2fb6da251c47ce9c440e86d754ff823eecab3d8c
Philipp T <philipp@virtualop.org>
2014-02-11 11:35:58 +0100

    copies tail(f)_service_logs to logfiles plugin, new parser apache_error

--- standard/logfiles/commands/list_parsers.rb
+++ standard/logfiles/commands/list_parsers.rb
@@ -2,6 +2,7 @@ add_columns [ :name, :type ]
 
 with_contributions do |result, params|
   result << { 'name' => 'xop_apache', 'type' => 'access_log' }
+  result << { 'name' => 'apache_error', 'type' => 'server_log' }
   result << { 'name' => 'log4j', 'type' => 'server_log' }
   result << { 'name' => 'jboss', 'type' => 'server_log' }
   result << { 'name' => 'ruby_logger', 'type' => 'server_log' }
