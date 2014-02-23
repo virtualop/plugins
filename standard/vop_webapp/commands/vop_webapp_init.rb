08b713d36e058d5d5a38a3f0bfed308a979e9843
Philipp T <philipp@virtualop.org>
2014-02-23 12:23:04 +0100

    vop_webapp_init: domain param and installing thin

--- standard/vop_webapp/commands/vop_webapp_init.rb
+++ standard/vop_webapp/commands/vop_webapp_init.rb
@@ -1,5 +1,7 @@
 param :machine, '', :default_value => 'self', :mandatory => false
-param 'path', 'service root of the webapp', :default_value => '/usr/lib/virtualop/webapp' 
+param 'path', 'service root of the webapp', :default_value => '/usr/lib/virtualop/webapp'
+param 'domain', 'the domain at which the vop webinterface should be available', 
+  :default_value => 'localhost', :default_param => true 
 
 on_machine do |machine, params|
   # TODO lets rather do this
@@ -26,4 +28,11 @@ on_machine do |machine, params|
     machine.rvm_ssh "cd #{service_root} && rake db:#{task}"
   end
   
+  machine.install_service_from_directory(
+    'directory' => service_root,
+    'service' => 'thin',
+    'extra_params' => {
+      'domain' => params['domain']
+    }
+  )
 end
