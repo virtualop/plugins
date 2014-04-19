e6907a144c5effa0701c99e7e395f49d8d193f49
Philipp T <philipp@virtualop.org>
2014-04-19 23:25:50 +0200

    generate_iptables-post_new_vm contributes diffrently, delete_stale_machine_entries extracted from webapp, vop_logging widget is loaded from plugin now

--- extended/iptables_generator/commands/generate_iptables_post_new_vm.rb
+++ extended/iptables_generator/commands/generate_iptables_post_new_vm.rb
@@ -1,7 +1,12 @@
-contributes_to :notify_new_vm_stop_ok
+#contributes_to :notify_new_vm_stop_ok
+contributes_to :notify_setup_vm_stop_ok
 
-param :machine
+accept_extra_params
 
-on_machine do |machine, params|
-  machine.generate_and_execute_iptables_script
-end
+execute do |params|
+  machine_name = params["extra_params"]["result"]
+  @op.generate_and_execute_iptables_script('machine' => machine_name)
+end  
+#on_machine do |machine, params|
+#  machine.generate_and_execute_iptables_script
+#end
--- standard/vop_webapp/commands/list_widgets.rb
+++ standard/vop_webapp/commands/list_widgets.rb
@@ -4,13 +4,27 @@ add_columns [ :name, :position ]
 
 with_contributions do |result, params|
   puts "current user : #{params['current_user']}"
-  pp params['current_user']
   
   result + [
     {
       'name' => 'machinez',
       'position' => 'main',
       'template' => @plugin.path + '/widgets/machinez.erb'
+    },
+    {
+      'name' => 'vop_logging',
+      'position' => 'north',
+      'template' => "#{Rails.root.to_s}/app/views/home/widgets/_vop_logging.erb"
+    },
+    {
+      'name' => 'machines',
+      'position' => 'fix',
+      'template' => "#{Rails.root.to_s}/app/views/home/widgets/_machines.erb"
+    },
+    {
+      'name' => 'projects',
+      'position' => 'fix',
+      'template' => "#{Rails.root.to_s}/app/views/home/widgets/_projects.erb"
     }
   ]  
 end
