bfe1ebda4e05f59e815f0f38e54845c94f03a1ef
Philipp T <philipp@hitchhackers.net>
2018-06-16 18:51:11 +0200

    install_service_async, params attribute for services entity

---
+++ standard/services/commands/install_service_async.rb
@@ -0,0 +1,9 @@
+param! :machine
+param! :service, default_param: true
+
+allows_extra
+
+run do |params|
+  request = ::Vop::Request.new(@op, "install_service", params)
+  @op.execute_async(request)
+end
--- standard/services/entities/service.rb
+++ standard/services/entities/service.rb
@@ -1,3 +1,8 @@
 entity do |plugin|
-  plugin.state[:services].map { |x| x.to_hash.merge("plugin_name" => x.plugin.name) }
+  plugin.state[:services].map do |x|
+    x.to_hash.merge(
+      "plugin_name" => x.plugin.name,
+      "params" => x.params
+    )
+  end
 end
