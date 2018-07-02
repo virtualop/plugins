bfac43aba86e1665d62ad656e323cc07a6757835
Philipp T <philipp@hitchhackers.net>
2018-07-02 21:19:17 +0200

    vm_installation_status pub/sub

--- standard/redis_machines/commands/track_installation_status.rb
+++ standard/redis_machines/commands/track_installation_status.rb
@@ -10,4 +10,10 @@ run do |host_name, vm_name, status, plugin|
   full_name = "#{vm_name}.#{host_name}"
   cache_key = "vop.installation_status.#{full_name}"
   redis.set(cache_key, status)
+
+  redis = plugin.state[:redis]
+  redis.publish("vm_installation_status", {
+    "machine" => full_name,
+    "status" => status
+  }.to_json())
 end
