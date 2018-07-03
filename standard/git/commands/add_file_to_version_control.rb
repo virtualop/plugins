bb2269cc1b165e91b753ac9a8f75fd9c9e2dd4ca
Philipp T <philipp@hitchhackers.net>
2018-07-03 22:23:39 +0200

    support multiple file_names in add_file_to_version_control

--- standard/git/commands/add_file_to_version_control.rb
+++ standard/git/commands/add_file_to_version_control.rb
@@ -2,10 +2,10 @@ description "registers a new file to the version control system so that it may b
 
 param! :machine
 param! "working_copy"
-param! "file_name", "the file that should be added"
+param! "file_name", description: "the file that should be added", multi: true
 
 run do |machine, working_copy, file_name|
-  machine.ssh "cd #{working_copy} && git add #{file_name}"
+  machine.ssh "cd #{working_copy} && git add #{file_name.join(" ")}"
 
   machine.git_status! working_copy: working_copy
 end
