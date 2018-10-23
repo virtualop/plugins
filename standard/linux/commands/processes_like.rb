dcb772f8794cd8d47ec5ee4cdb2d5a8069f09f67
Philipp T <philipp@hitchhackers.net>
2018-10-23 20:58:20 +0200

    processes!

--- standard/linux/commands/processes_like.rb
+++ standard/linux/commands/processes_like.rb
@@ -5,7 +5,7 @@ show columns: %w|pid command|
 
 run do |machine, regex|
   the_regex = /#{Regexp.escape(regex)}/
-  machine.processes.select do |process|
+  machine.processes!.select do |process|
     the_regex.match(process["command"])
   end
 end
