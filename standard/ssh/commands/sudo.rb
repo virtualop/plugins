442339577b7059c7c40a6d3dd00b0baaf765172c
Philipp T <philipp@hitchhackers.net>
2019-02-14 22:20:00 +0100

    sudo.show_output

--- standard/ssh/commands/sudo.rb
+++ standard/ssh/commands/sudo.rb
@@ -1,12 +1,15 @@
 param! :machine
 param! "command", default_param: true
 
-run do |machine, params|
+param "show_output", default: false
+
+run do |machine, params, show_output|
   sudo_options = ""
   distro = machine.distro.split("_").first
   if %w|debian ubuntu|.include? distro
     sudo_options += "--non-interactive"
   end
 
-  machine.ssh("command" => "sudo #{sudo_options} #{params["command"]}", "request_pty" => true)
+  machine.ssh "command" => "sudo #{sudo_options} #{params["command"]}",
+    "request_pty" => true, "show_output" => show_output
 end
