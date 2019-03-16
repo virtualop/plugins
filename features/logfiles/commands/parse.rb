ed108c3860baf04d6c8032c918334b66c3f10de5
Philipp T <philipp@hitchhackers.net>
2019-03-16 22:34:22 +0100

    fix parse

--- features/logfiles/commands/parse.rb
+++ features/logfiles/commands/parse.rb
@@ -7,14 +7,14 @@ dont_log
 run do |machine, log, data|
   parser = log["parser"]
   if parser.nil?
-    raise "no parser definition found for log file #{path} (service probably : #{log["source"]})"
+    raise "no parser definition found for log file #{log.path} (service probably : #{log["source"]})"
   end
 
   result = nil
   if @op.respond_to?(parser)
     result = @op.send(parser.to_sym, data)
   else
-    raise "parser command defined for log #{path} in #{log["source"]} does not seem to be registered in this vop instance."
+    raise "parser command '#{parser}' defined for log #{log.path} in #{log["source"]} does not seem to be registered in this vop instance"
   end
   result
 end
