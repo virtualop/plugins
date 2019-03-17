768f49908f25d82f1b54c0cfa12f699fe056cec7
Philipp T <philipp@hitchhackers.net>
2019-03-17 15:58:46 +0100

    add SSL headers to config generated by letsencrypt

--- services/certbot/commands/letsencrypt.rb
+++ services/certbot/commands/letsencrypt.rb
@@ -7,7 +7,7 @@ run do |plugin, machine, domain|
     raise "missing configuration key 'eff_email'"
   end
 
-  certbot_bin = "certbot"  
+  certbot_bin = "certbot"
 
   domain.each do |d|
     certbot_cmd = "#{certbot_bin} --non-interactive -m #{certbot_email} --agree-tos --eff-email"
@@ -18,4 +18,27 @@ run do |plugin, machine, domain|
   # invalidate
   machine.list_files! "/etc/apache2/sites-enabled"
   machine.list_files! "/etc/apache2/sites-available"
+
+  # post process: add SSL headers
+  domain.each do |d|
+    config = "/etc/apache2/sites-available/#{d}-le-ssl.conf"
+    if machine.file_exists(config)
+      file = machine.read_file!(config).split("\n")
+
+      unless file.index "RequestHeader set X-Forwarded-Proto https"
+        pos = file.index "ProxyPreserveHost On"
+        file.insert pos + 1, "RequestHeader set X-Forwarded-Proto https"
+        file.insert pos + 1, "RequestHeader set X-Forwarded-Ssl on"
+
+        machine.write_file(
+          file_name: config,
+          sudo: true,
+          content: file.join("\n")
+        )
+        machine.read_file! config
+      end
+    else
+      $logger.warn("did not find config file #{config}, can not postprocess. #sad")
+    end
+  end
 end
