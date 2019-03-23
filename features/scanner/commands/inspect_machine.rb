85d0eeaed3812b71e05396877ced6e831eb2d2a5
Philipp T <philipp@hitchhackers.net>
2019-03-23 19:06:15 +0100

    60s timeout for inspect_machine

--- features/scanner/commands/inspect_machine.rb
+++ features/scanner/commands/inspect_machine.rb
@@ -1,66 +1,74 @@
 param! :machine
 
 run do |plugin, machine|
+  now = Time.now.utc.to_i
   result = {
     "ssh_status" => "unknown",
     "internal_ip" => "unknown",
+    "scan_from" => now
   }
-
-  machine_type = machine.metadata["type"]
-  if machine_type == "vm"
-    # make sure the internal IP we've got is up to date
-    machine.parent.vm_addresses!(name: machine.name.split(".").first)
-  end
-  machine.ssh_options!
 
   begin
-    result["ssh_status"] = machine.test_ssh!
-  rescue => e
-    $logger.info "error testing ssh connect to #{machine.name} : #{e.message}"
-    if e.message =~ /no SSH options/
-      ssh_error = "no_ssh_options"
-    else
-      ssh_error = e.message
-    end
-    result["ssh_error"] = ssh_error
-  end
-
-  now = Time.now.utc.to_i
-  result["scan_from"] = now
-
-  machine.update_metadata new_data: {
-    "ssh_status" => result["ssh_status"],
-    "ssh_status_from" => now
-  }
-
-  if result["ssh_status"]
-    begin
-      result["internal_ip"] = machine.internal_ip
-    rescue => e
-      $logger.error "problem reading internal IP from #{machine.name} : #{e.message}"
-    end
-
-    result["distro"] = machine.distro
-
-    garbled = machine.processes!
-    ungarbled = garbled.map do |row|
-      h = { }
-      row.each do |k,v|
-        h[k] = v.force_encoding("UTF-8")
+    Timeout::timeout(60) do
+      machine_type = machine.metadata["type"]
+      if machine_type == "vm"
+        # make sure the internal IP we've got is up to date
+        machine.parent.vm_addresses!(name: machine.name.split(".").first)
       end
-      h
+      machine.ssh_options!
+
+      begin
+        result["ssh_status"] = machine.test_ssh!
+      rescue => e
+        $logger.info "error testing ssh connect to #{machine.name} : #{e.message}"
+        if e.message =~ /no SSH options/
+          ssh_error = "no_ssh_options"
+        else
+          ssh_error = e.message
+        end
+        result["ssh_error"] = ssh_error
+      end
+
+      now = Time.now.utc.to_i
+      result["scan_from"] = now
+
+      machine.update_metadata new_data: {
+        "ssh_status" => result["ssh_status"],
+        "ssh_status_from" => now
+      }
+
+      if result["ssh_status"]
+        begin
+          result["internal_ip"] = machine.internal_ip
+        rescue => e
+          $logger.error "problem reading internal IP from #{machine.name} : #{e.message}"
+        end
+
+        result["distro"] = machine.distro
+
+        garbled = machine.processes!
+        ungarbled = garbled.map do |row|
+          h = { }
+          row.each do |k,v|
+            h[k] = v.force_encoding("UTF-8")
+          end
+          h
+        end
+        result["processes"] = ungarbled
+
+        machine.processes_top_mem
+
+        result["services"] = machine.detect_services!
+
+        # TODO packages contain non-serializable characters (like processes above)
+        # result["packages"] = machine.list_packages
+        if machine.detect_services!.include? "apache.apache"
+          machine.vhosts!
+        end
+      end
     end
-    result["processes"] = ungarbled
-
-    machine.processes_top_mem
-
-    result["services"] = machine.detect_services!
-
-    # TODO packages contain non-serializable characters (like processes above)
-    # result["packages"] = machine.list_packages
-    if machine.detect_services!.include? "apache.apache"
-      machine.vhosts!
-    end
+  rescue => detail
+    raise "timed out while inspecting #{machine.name} : #{detail.message}"
   end
 
   redis = plugin.state[:redis]
