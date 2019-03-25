0293175b1f7fdbecbba8871d18d35f68ab3facd6
Philipp T <philipp@hitchhackers.net>
2019-03-25 11:31:35 +0100

    check installation status in list_usable_spares

--- features/spares/commands/list_usable_spares.rb
+++ features/spares/commands/list_usable_spares.rb
@@ -7,7 +7,13 @@ run do |machine|
     if vm.nil?
       raise "sanity check failed : vm #{vm_name} nil"
     end
-    vm["state"] == "running" &&
-    @op.test_ssh("machine" => "#{vm_name}.#{machine.name}") ? vm_name : nil
+
+    vm_running = vm["state"] == "running"
+    ssh_ok = @op.test_ssh("machine" => "#{vm_name}.#{machine.name}")
+    installation_finished = @op.installation_status(host_name: machine.name, vm_name: vm_name) == "installed"
+
+    usable = vm_running && installation_finished && ssh_ok
+
+    usable ? vm_name : nil
   end.compact
 end
