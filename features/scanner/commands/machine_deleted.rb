23c7c0461705368ef421ef703adaf5d480139fa7
Philipp T <philipp@hitchhackers.net>
2019-04-05 07:59:43 +0200

    machine_deleted now open for contributions

--- features/scanner/commands/machine_deleted.rb
+++ features/scanner/commands/machine_deleted.rb
@@ -3,6 +3,11 @@ param! "machine"
 run do |machine, plugin|
   redis = plugin.state[:redis]
 
+  @op.collect_contributions(
+    command_name: "machine_deleted",
+    raw_params: {}
+  )
+
   redis.lrem(machine_list_key(), 0, machine)
 
   @op.redis_machines!
