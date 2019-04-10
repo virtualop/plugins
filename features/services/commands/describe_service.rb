356272b2001e6c90ae083fceac166caec847abaa
Philipp T <philipp@hitchhackers.net>
2019-04-10 14:08:02 +0200

    include installation record into describe_service when called on a machine

--- features/services/commands/describe_service.rb
+++ features/services/commands/describe_service.rb
@@ -1,7 +1,17 @@
 param! :known_service
+param :machine
 
 show display_type: :data
 
-run do |plugin, known_service|
-  known_service.data
+run do |plugin, known_service, machine|
+  result = known_service.data
+
+  if machine
+    installation_params = machine.installed_services[known_service.name].data
+    result.merge!({
+      "installed" => installation_params
+    })
+  end
+
+  result
 end
