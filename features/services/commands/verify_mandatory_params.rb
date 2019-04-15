19b270283fff0211b48eac853fbc7281d2ed61ef
Philipp T <philipp@hitchhackers.net>
2019-04-15 14:52:42 +0200

    allow symbolic service param names

--- features/services/commands/verify_mandatory_params.rb
+++ features/services/commands/verify_mandatory_params.rb
@@ -7,7 +7,7 @@ run do |plugin, machine, known_service, params|
   detail = known_service.describe_service
   detail["params"].each do |svc_param|
     if svc_param.has_key?("options") && svc_param["options"]["mandatory"]
-      unless params.has_key?(svc_param["name"])
+      unless params.has_key?(svc_param["name"]) || params.has_key?(svc_param["name"].to_sym)
         raise "missing param '#{svc_param["name"]}'"
       end
     end
