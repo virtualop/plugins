2981c4a67b50af8a4d3ba818d74cbb560be16098
Philipp T <philipp@hitchhackers.net>
2019-04-27 10:02:26 +0200

    too stupid to watch memory

--- services/linux/commands/watch_memory.rb
+++ services/linux/commands/watch_memory.rb
@@ -1,27 +1,12 @@
 param! :machine
-param! "count", default: 60
+param "interval", default: 2
 
-run do |plugin, machine, count|
+run do |plugin, params, machine, interval|
   redis = plugin.state[:redis]
 
-  # result = machine.ssh_call(
-  #   "command" => "free -m",
-  #   "dont_loop" => true,
-  #   "request_pty" => true,
-  #   "on_data" => lambda { |c, data|
-  #     puts "got data : #{data}"
-  #     parsed = @op.parse_memory(data)
-  #
-  #     redis.publish("memory_updates", {
-  #       "machine" => machine.name,
-  #       "content" => parsed
-  #     }.to_json())
-  #   },
-  #   "on_stderr" => lambda { |c, data| puts data }
-  # )
-  # result["connection"].loop
+  interval = interval.to_i
 
-  1.upto(count) do
+  while true do
     input = machine.ssh("free -m")
     parsed = @op.parse_memory(input)
 
@@ -30,6 +15,6 @@ run do |plugin, machine, count|
       "content" => parsed
     }.to_json())
 
-    sleep 1
+    sleep interval
   end
 end
